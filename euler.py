3a317d6010756208b6efb52b12dd84e872016b6d
TranHoangViet <viet14042000@gmail.com>
2020-07-13 14:39:59 +0700

    update sir, sird euler, parser

--- euler.py
+++ euler.py
@@ -1,87 +1,97 @@
-import optparse
-import errorHandler
-import os
 import sys
+import os
+import errorHandler
+import argparse
 
-def readCommand(argv):
-    parser = optparse.OptionParser()
-    parser.add_option('-m','--model',
-                      dest='model',
-                      help='Model SIR or SIRD. this option is requirement.')
-    parser.add_option('-t','--testcase',
-                      dest='testcase',
-                      help='You just type the filename in testcase/Euler/ directory. This option is requirement.')
-    parser.add_option('-s','--step',
-                      type='int',
-                      dest='step',
-                      default=8,
-                      help='The steps you want to run your algorithm. Default is 8')                  
-    (options, _) = parser.parse_args(argv)
-    return options
 
-def Euler_SIR(filename,step):
+def readCommand():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model',
+                        required=True,
+                        default=None,
+                        help='Model SIR or SIRD. this option is requirement.')
+    parser.add_argument('-t', '--testcase',
+                        required=True,
+                        default=None,
+                        help='You just type the filename in testcase/Euler/ directory. This option is requirement.')
+    parser.add_argument('-s', '--step',
+                        type=int,
+                        required=False,
+                        default=8,
+                        help='The steps you want to run your algorithm. Default is 8. Steps is the number of interval time from the beginning time to the end time')
+    return parser
+
+
+def Euler_SIR(filename, step):
     testcase = _readEulerTestcase(filename)
-    t, B, g, S, I, R = testcase
+    dentaT, B, g, S, I, R = testcase
     result = []
-    result.append((S,I,R))
+    result.append((0, S, I, R))
     for i in range(step):
-        dS = -B*I*S
-        dI = B*I*S - g*I
-        dR = g*I
+        dS = (-B*I*S) * dentaT
+        dI = (B*I*S - g*I) * dentaT
+        dR = (g*I) * dentaT
         S = S + dS
         I = I + dI
         R = R + dR
-        result.append((S,I,R))
-    _printEulerSIRData(result,t)
+        result.append((0+i*dentaT, S, I, R))
+    _printEulerSIRData(result)
     return result
-def _printEulerSIRData(data,deltaT = 1):
+
+
+def _printEulerSIRData(data):
     print("Ngày\tCó nguy cơ\tCa nhiễm\tCa hồi phục")
     for i in range(len(data)):
-        print(int(i*deltaT),"\t %0.5f"%data[i][0],"\t %0.5f"% data[i][1],"\t %0.5f"%data[i][2])
+        print(data[i][0], "\t %0.5f" %
+              data[i][1], "\t %0.5f" % data[i][2], "\t %0.5f" % data[i][3])
 
-def Euler_SIRD(filename,step):
+
+def Euler_SIRD(filename, step):
     testcase = _readEulerTestcase(filename)
-    t, B, g, u, S, I, R, D= testcase
+    dentaT, B, g, u, S, I, R, D = testcase
     result = []
-    result.append((S,I,R,D))
+    result.append((0, S, I, R, D))
     for i in range(step):
-        dS = -B*I*S
-        dI = B*I*S - g*I -u*I
-        dR = g*I
-        dD = u*I
+        dS = (-B*I*S) * dentaT
+        dI = (B*I*S - g*I - u*I) * dentaT
+        dR = (g*I)*dentaT
+        dD = (u*I)*dentaT
         S = S + dS
         I = I + dI
         R = R + dR
         D = D + dD
-        result.append((S,I,R,D))
-    _printEulerSIRDData(result,t)
+        result.append((0+i*dentaT, S, I, R, D))
+    _printEulerSIRDData(result)
     return result
-def _printEulerSIRDData(data,deltaT = 1):
+
+
+def _printEulerSIRDData(data, deltaT=1):
     print("Ngày\tCó nguy cơ\tCa nhiễm\tCa hồi phục\tCa chết")
     for i in range(len(data)):
-        print(int(i*deltaT),"\t %0.5f"%data[i][0],"\t %0.5f"% data[i][1],"\t %0.5f"%data[i][2],"\t %0.5f"%data[i][3])
+        print(data[i][0], "\t %0.5f" % data[i][1], "\t %0.5f" %
+              data[i][2], "\t %0.5f" % data[i][3], "\t %0.5f" % data[i][4])
+
 
 def _readEulerTestcase(filename):
-    f=open(filename,'r')
+    f = open(filename, 'r')
     result = []
-    l=f.readline()
+    l = f.readline()
     while(l):
-        l=l.replace("\n","")
+        l = l.replace("\n", "")
         result.append(float(l))
-        l=f.readline()
+        l = f.readline()
     return result
 
+
 if __name__ == '__main__':
-    options = readCommand(sys.argv)
-    if(options.model == "SIR"):
-        if(options.testcase == None):
-            errorHandler.commandMissing("--testcase")
-        else:
-            Euler_SIR("./testcase/Euler/"+options.testcase,options.step)
-    elif(options.model == "SIRD"):
-        if(options.testcase == None):
-            errorHandler.commandMissing("--testcase")
-        else:
-            Euler_SIRD("./testcase/Euler/"+options.testcase,options.step)
+    try:
+        options = readCommand().parse_args()
+    except:
+        readCommand().print_help()
+        sys.exit(0)
+    if options.model == 'SIR':
+        Euler_SIR('./testcase/Euler/'+options.testcase,
+                  options.step)
     else:
-        errorHandler.commandMissing("--model")
+        Euler_SIRD('./testcase/Euler/'+options.testcase,
+                   options.step)
