0956a92e820279d5589d338f6e2928dc2e798e23
TungKhanhTran <tung.tran.1805@hcmut.edu.vn>
2020-07-19 23:34:33 +0700

    Done problem4 but there sure is a lot of bugs :)

--- MetropolisHastings.py
+++ MetropolisHastings.py
@@ -9,7 +9,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def proposal_distribution_func(Beta, Gamma):
+mean = np.array([0.5, 0.5])
+cov = np.array([[1.0, 0.7],
+                [0.7,1.2]])
+
+def proposalDistribution(Beta, Gamma):
     # probability distribution function
     # normal distribution
     return [Beta, Gamma] + np.random.normal(size=2)
@@ -18,31 +22,74 @@ def proposal_distribution_func(Beta, Gamma):
 def pFunc(xStar, x):
     return st.multivariate_normal.pdf(xStar, x)
 
+lambdaBeta= 0.5
+nuBeta = 0.1
+lambdaGamma= 0.5
+nuGamma = 0.1
 
-def piFunc(x):
-    # probability density function
-    return st.multivariate_normal.pdf(x, [1.4, 0.4])
+def gammaDistribution(Beta,Gamma):
+    """
+    param   Beta    : float
+            Gamma   : float
+    return π(β, γ) with gamma distribution
+    """
+    # π(β,γ) = π(β)* π(γ)oj
+    # piBeta = ((nuBeta**lambdaBeta)*(Beta**(lambdaBeta-1))*(e**(-nuBeta*Beta)))/gamma(lambdaBeta)
+    # piGamma = ((nuGamma**lambdaGamma)*(Gamma**(lambdaGamma-1))*(e**(-nuGamma*Gamma)))/gamma(lambdaGamma)
+    piBeta = st.gamma.pdf(Beta,a=lambdaBeta,scale = 1/nuBeta)
+    piGamma = st.gamma.pdf(Gamma,a=lambdaGamma,scale = 1/nuGamma)
+    return piBeta*piGamma
+def gaussDistribution(Beta,Gamma):
+    """
+    param   Beta    : float
+            Gamma   : float
+    return π(β, γ) with gauss distribution
+    """
+    return st.multivariate_normal.pdf((Beta,Gamma), mean,cov)
 
 
-def metropolis_hastings(iter, piFunc, pFunc, init):
-    Beta, Gamma = init
+def metropolisHastings(iter, piFunc, proposalDistribution = proposalDistribution, pFunc=None):
+    Beta0, Gamma0 = 1.,1.
+    # Khởi tạo β0 và γ0 từ phân bố xác suất tiên nghiệm π(β, γ).
+    # bằng cách sử dụng giải thuật Metropolis Hastings cho đến khi xác suất đủ lớn để chấp nhận.
+    # hay còn gọi là chạy giải thuật cho đến khi tìm được một cặp (β, γ) thỏa phân bố xác suất tiên nghiệm π(β, γ)
+    while (piFunc(Beta0,Gamma0) < 5e-5):
+        BetaStar, GammaStar = proposalDistribution(Beta0, Gamma0)
+        # vì proposalDistribution là phân phối xác suất đối xứng vì thế ta rút gọn tính toán
+        r = min(1, piFunc(BetaStar, GammaStar) / (piFunc(Beta0, Gamma0)))
+
+        q = np.random.rand()
+        if q < r:
+            Beta0, Gamma0 = BetaStar, GammaStar
     samples = np.zeros((iter, 2))
-    for i in range(iter):
+    samples[0]= np.array([Beta0,Gamma0])
+    # bắt đầu bước 2
+    for i in range(iter-1):
+        # gán β := βi và γ := γi
+        Beta, Gamma = samples[i]
+
+        # Khởi tạo β* và γ* ngẫu nhiên từ phân phối xác suất bất kỳ p(β, γ).
         # Phân phối chuẩn là phân phối có tính chất đối xứng
         # Một vài phân phối khác cũng có tính đối xứng: Cauchy distribution, logistic distribution, uniform distribution
-        BetaStar, GammaStar = proposal_distribution_func(Beta, Gamma)
+        BetaStar, GammaStar = proposalDistribution(Beta, Gamma)
+
+        # vì proposalDistribution là phân phối xác suất đối xứng vì thế ta rút gọn tính toán
+        # r = min(1, piFunc(BetaStar, GammaStar) * pFunc((BetaStar, GammaStar), (Beta, Gamma)) / (piFunc(Beta, Gamma)*pFunc((Beta, Gamma), (BetaStar, GammaStar))))
+        r = min(1, piFunc(BetaStar, GammaStar) / (piFunc(Beta, Gamma)))
+
+        # khởi tạo q từ phân phối đều liên tục U(0,1)
         q = np.random.rand()
-        r = min(1, piFunc((BetaStar, GammaStar)) * pFunc((BetaStar, GammaStar), (Beta,
-                                                                                 Gamma)) / (piFunc((Beta, Gamma))*pFunc((Beta, Gamma), (BetaStar, GammaStar))))
+        
         if q < r:
             Beta, Gamma = BetaStar, GammaStar
-        samples[i] = np.array([Beta, Gamma])
+        # nếu q < r thì gán (β_i+1, γ_i+1) = (β*, γ*)
+        # ngược lại thì gán (β_i+1, γ_i+1) = (β, γ)
+        samples[i+1] = np.array([Beta, Gamma])
     return samples
 
 
 if __name__ == '__main__':
-    init = np.array([3, 4])
-    result = metropolis_hastings(10000, piFunc, pFunc, init)
+    result = metropolisHastings(10000, gammaDistribution)
     sns.jointplot(x=result[:, 0], y=result[:, 1])
     print(result)
     plt.show()
---
+++ rzerocoeff.py
@@ -0,0 +1,75 @@
+import sys
+import os
+import errorHandler
+import argparse
+import numpy as np
+from math import *
+import scipy.stats as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+from MetropolisHastings import metropolisHastings, gammaDistribution, pFunc, gaussDistribution
+
+# from MetropolisHastings import metropolisHastings
+import pandas as pd
+
+# Hàm hiện thực công thức 15:
+def calPosterior(X, Beta, Gamma):
+    product = 1.
+    for Xi in X:
+        try:
+            if Xi != 0:
+                # print(Gamma, Beta)
+                product *= (pow(Gamma, Beta)/gamma(Beta) * pow(Xi, (Beta - 1)) * pow(e, (-Gamma * Xi)))
+        # else:
+        #     product = 0
+        #     break
+        # print(pow(e, -Gamma * Xi))
+        # if isnan(product):
+        #     print("sdoishdsisihdsdi ", gamma(Beta), e ** (-Gamma * Xi), ' ', Gamma * Xi)
+        #     # break
+        #     product = 0
+        # product *= st.gamma.pdf(Xi,a=Beta,scale= 1/Gamma)
+            # print(product)
+        except ValueError:
+            # pass
+            print(Gamma, Beta, Xi)
+        # except OverflowError:
+        #     product = 0
+    # product *= ((Gamma ** Beta)/gamma(Beta) ** len(X))
+    # print(product)
+    return product
+
+# Hàm hiện thực công thức 21:
+def calCoeff(X, samples):
+    rzero = 0
+    normalizedSamples = np.zeros(samples.shape)
+    normalizedSamples[:,0] = (samples[:,0] - np.min(samples[:,0]))/np.ptp(samples[:,0])
+    normalizedSamples[:,1] = (samples[:,1] - np.min(samples[:,1]))/np.ptp(samples[:,1])
+    # print(normalizedSamples)
+    # print(rzero)
+    for sample in normalizedSamples:
+        # print(sample)
+        if sample[1] != 0 and sample[0] != 0:
+            rzero += (calPosterior(X, sample[0], sample[1]) * sample[0]/sample[1])
+        # print(rzero)
+    return rzero
+
+if __name__ == '__main__':
+    url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+    df = pd.read_csv(url, error_bad_lines=False)
+    # Chọn các quốc gia: Germany (122), France (118), United Kingdom (225)
+    indexes = [116, 120, 223]
+    coeffs = list()
+    df = df.iloc[indexes,0:14]
+    # print(df.isna())
+    for iter in df.iterrows():
+        # Đi từ index 5 tương ứng ngày đầu tiên 01/22/20
+        X = iter[1][5:].tolist()
+        # X_norm = [float(i)/500000 for i in X]
+        # print(X)
+
+        samples = metropolisHastings(10000, gammaDistribution)
+
+        # print(samples)
+        coeffs.append(calCoeff(X, samples))
+    print(coeffs)
